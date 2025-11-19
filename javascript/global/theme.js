416ccdf50ef73f80157b3a6eacd1d19f4a0f4659
Clonyx1 <korinekluky@proton.me>
2025-11-19 10:37:40 +0100

    Vylepšen theme-switch, změny menu navigace
    -Stránka si nově pamatuje jaké téma si uživatel zvolil napříč všemi stránkami
    
    -Oprava mezer v navigaci

--- javascript/global/theme.js
+++ javascript/global/theme.js
@@ -1,7 +1,20 @@
+const storageKey = "themeSwitch";
 const themeBtn = document.getElementById('theme-switch');
+
+if(sessionStorage.getItem(storageKey) === 'true'){
+    document.documentElement.classList.toggle('dark-theme');
+    themeBtn.checked = 'true';
+}
+
 
 if(themeBtn){
     themeBtn.addEventListener('change', () => {
-        document.documentElement.classList.toggle('dark-theme')
+        document.documentElement.classList.toggle('dark-theme');
+        if(sessionStorage.getItem(storageKey) === 'true'){
+            sessionStorage.setItem(storageKey, 'false');
+        }
+        else{
+            sessionStorage.setItem(storageKey, 'true');
+        }
     })
 }
