3164429935b1c65e94ee143a33f6e249a89e5862
Clonyx1 <korinekluky@proton.me>
2025-12-04 13:32:24 +0100

    Odstraněn nepoužívaný javascript, přidány alt obrázků

--- javascript/components/productCards.js
+++ javascript/components/productCards.js
@@ -17,23 +17,19 @@ async function addElement() {
     const productSectionGrid = document.getElementById('product-section');
 
     data.forEach(record => {
-        const element = document.createElement("div");
-        element.classList.add("product-card");
-        element.classList.add("pd-2");
-        element.classList.add("mr-2");
+        let element = document.createElement("div");
+        elementClasses = ["product-card", "pd-2", "mr-2"];
+        element = addProperties(element, elementClasses);
 
         //Card heading
-        const cardHeading = document.createElement("h3");
-        cardHeading.innerText = record.name;
-        cardHeading.classList.add("product-card-heading");
-        cardHeading.classList.add("pd-t-3");
-        cardHeading.classList.add("pd-b-3");
+        let cardHeading = document.createElement("h3");
+        cardHeadingClasses = ["product-card-heading", "pd-t-3", "pd-b-3"];
+        cardHeading = addProperties(cardHeading, cardHeadingClasses, record.name);
 
         //Card text
-        const cardText = document.createElement("p");
-        cardText.innerText = record.text;
-        cardText.classList.add("product-card-text");
-        cardText.classList.add("mr-b-3");
+        let cardText = document.createElement("p");
+        cardTextClasses = ["product-card-text", "mr-b-3"];
+        cardText = addProperties(cardText, cardTextClasses, record.text);
 
         //Card price
         const cardPrice = document.createElement("p");
@@ -49,6 +45,7 @@ async function addElement() {
 
         const img = document.createElement("img");
         img.src = record.imgSrc;
+        img.alt = record.imgAlt;
 
         cardPicture.appendChild(imgSource);
         cardPicture.appendChild(img);
@@ -61,37 +58,17 @@ async function addElement() {
 
         productSectionGrid.appendChild(element);
     });
-    /*
-    //Main div of the card
-    const element = document.createElement("div");
-    element.classList.add("product-card");
-    element.classList.add("pd-2");
-    element.classList.add("mr-2");
-    //Card heading
-    const cardHeading = document.createElement("h2");
-    cardHeading.innerText = "Produkt";
-    cardHeading.classList.add("product-card-heading");
-    cardHeading.classList.add("pd-t-3");
-    cardHeading.classList.add("pd-b-3");
-    //Card text
-    const cardText = document.createElement("p");
-    cardText.innerText = "Poctivý domácí chléb s křupavou kůrkou a měkkou střídou. Pečený s láskou z kvalitní mouky a tradičního kvásku.";
-    cardText.classList.add("product-card-text");
-    cardText.classList.add("mr-b-3");
-    //Card price
-    const cardPrice = document.createElement("p");
-    cardPrice.innerText = "25Kč,-";
-    cardPrice.classList.add("product-card-price");
-    //Card image
-    const cardImg = document.createElement("img");
-    cardImg.src = "obrazky/bageta.avif";
-    cardImg.classList.add("product-card-img");
+}
 
-    element.appendChild(cardImg);
-    element.appendChild(cardHeading);
-    element.appendChild(cardText);
-    element.appendChild(cardPrice);
+function addProperties(element, classes, innerText) {
+    classes.forEach(c => {
+        element.classList.add(c);
+    });
+
+    if (innerText) {
+        element.innerText = innerText;
+    }
 
 
-    main.appendChild(element);*/
+    return element;
 }
